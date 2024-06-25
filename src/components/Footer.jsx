fbcaf0586ae52696a977afa5de9d3b0753f5179d
Dave Lim <davelim77@gmail.com>
2024-06-25 15:54:26 -0500

    Generated Vite project + some minor FCC changes

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Footer() {
+    return (
+      <footer className="footer">
+        <small>&copy; 2024 Lim development. All rights reserved.</small>
+      </footer>
+    )
+}
+export default Footer;
