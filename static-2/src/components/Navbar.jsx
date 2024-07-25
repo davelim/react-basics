baa2e54149a730b278322cb0ace451848e4fafed
Dave Lim <davelim77@gmail.com>
2024-07-25 15:23:04 -0500

    Mostly CSS styling

--- static-2/src/components/Navbar.jsx
+++ static-2/src/components/Navbar.jsx
@@ -1,5 +1,14 @@
+import logo from "../assets/react.svg";
+
+
 export default function Navbar() {
     return (
-        <h1>navbar goes here</h1>
+        <header>
+            <nav className="nav">
+                <img src={logo} className="nav--icon"/>
+                <h3 className="nav--logo_text">ReactFacts</h3>
+                <h4 className="nav--title">React Course - Project 1</h4>
+            </nav>
+        </header>
     )
 }
