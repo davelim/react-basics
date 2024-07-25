7fe7e61a84ae924a15da339a200684890ae76ed9
Dave Lim <davelim77@gmail.com>
2024-07-25 13:55:51 -0500

    High-level layout

---
+++ static-2/src/App.jsx
@@ -0,0 +1,18 @@
+
+import Navbar from "./components/Navbar";
+import Main from "./components/Main";
+
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <div className="container">
+      <Navbar />
+      <Main />
+    </div>
+  )
+}
+
+export default App
