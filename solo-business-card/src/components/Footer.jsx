fc613913eb559b7dfa2ca7b60d3ce93bac19309e
Dave Lim <davelim77@gmail.com>
2024-07-29 12:47:05 -0500

    More styling to match "requirements"
    Some highlights:
    - use google "inter" font
    - use font awesome icons
    - a bit of fiddling with margins, paddings, sizes and colors

--- solo-business-card/src/components/Footer.jsx
+++ solo-business-card/src/components/Footer.jsx
@@ -1,12 +1,14 @@
 import twitter from "../assets/twitter.png";
+import facebook from "../assets/facebook-icon.jpg";
+import instagram from "../assets/instagram-icon.png";
 import github from "../assets/github.png";
 
 export default function Footer() {
     return (
         <div className="footer--div">
             <a href="/"><img src={twitter} alt="twitter-logo" /></a>&nbsp;
-            <a>todo: facebook icon</a>&nbsp;
-            <a>todo: instagram icon</a>&nbsp;
+            <a href="/"><img src={facebook} alt="facebook-logo"/></a>&nbsp;
+            <a href="/"><img src={instagram} alt="instagram-logo"/></a>&nbsp;
             <a href="https://github.com/davelim" target="_blank"><img src={github} alt="github-logo" /></a>
         </div>
     );
--- solo-business-card/src/components/Info.jsx
+++ solo-business-card/src/components/Info.jsx
@@ -1,16 +1,35 @@
 import photo from "../assets/glasses-dude.jpg";
 
-
+const emailButtonStyle = {
+    backgroundColor: "white",
+}
+const linkedInButtonStyle = {
+    // https://encycolorpedia.com/0072b1#google_vignette
+    backgroundColor: "#1688c7",
+    color: "white",
+}
+const linkedInIconStyle = {
+    color: "#1688c7",
+    backgroundColor: "white",
+    padding: "3px",
+    borderRadius: "2px",
+}
 export default function Info() {
     return (
-        <div >
+        <div className="info">
             <img src={photo} className="info--photo"/>
-            <h2>John Smith</h2>
-            <h4>Frontend Developer</h4>
-            <span>johnsmith.website</span>
-            <div>
-                <button>Email</button>
-                <button>LinkedIn</button>
+            <h2 className="info--name">John Smith</h2>
+            <span className="info--job">Frontend Developer</span><br></br>
+            <span className="info--website">johnsmith.website</span>
+            <div className="info--contact">
+                <button style={emailButtonStyle}>
+                    <i className="fa fa-envelope"/>&nbsp;
+                    Email
+                </button>
+                <button style={linkedInButtonStyle}>
+                    <i className="fa fa-linkedin" style={linkedInIconStyle}/>&nbsp;
+                    LinkedIn
+                </button>
             </div>
         </div>
     );
