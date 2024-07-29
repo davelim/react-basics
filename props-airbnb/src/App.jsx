40bb528b6d895dcdf30270ccb31284cc68d9de6a
Dave Lim <davelim77@gmail.com>
2024-07-29 14:29:26 -0500

    Nav bar

--- props-airbnb/src/App.jsx
+++ props-airbnb/src/App.jsx
@@ -1,9 +1,11 @@
+import Navbar from "./components/Navbar";
 import './App.css'
 
 function App() {
 
   return (
     <>
+      <Navbar />
       <h1>App component</h1>
     </>
   )
---
+++ props-airbnb/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import logo from "../assets/Airbnb-logo.png";
+
+export default function Navbar() {
+    return (
+        <header>
+            <nav className="nav">
+                <img src={logo} />
+            </nav>
+        </header>
+    )
+}
