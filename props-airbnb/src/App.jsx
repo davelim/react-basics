263bf57d4a2944645d0b43d91c879821f30b1c1a
Dave Lim <davelim77@gmail.com>
2024-07-30 15:43:11 -0500

    Props for contact card

--- props-airbnb/src/App.jsx
+++ props-airbnb/src/App.jsx
@@ -14,10 +14,26 @@ function App() {
       <Hero />
       <Card />
       <div className="contacts">
-        <ContactCard />
-        <ContactCard />
-        <ContactCard />
-        <ContactCard />
+        <ContactCard
+          img="../assets/mr-whiskerson.jpg"
+          name="Mr. Whiskerson"
+          phoneNumber="(212) 555-1234"
+          email="mr.whiskaz@catnap.meow"/>
+        <ContactCard
+          img="../assets/fluffykins.jpg"
+          name="Fluffykins"
+          phoneNumber="(212) 555-2345"
+          email="fluff@me.com"/>
+        <ContactCard
+          img="../assets/felix.jpg"
+          name="Felix"
+          phoneNumber="(212) 555-4567"
+          email="thecat@hotmail.com"/>
+        <ContactCard
+          img="../assets/pumpkin.png"
+          name="Pumpkin"
+          phoneNumber="(0800) CAT KING"
+          email="pumpkin@scrimba.com"/>
       </div>
     </div>
   )
--- props-airbnb/src/components/ContactCard.jsx
+++ props-airbnb/src/components/ContactCard.jsx
@@ -1,20 +1,23 @@
-import whiskerson from "../assets/mr-whiskerson.jpg";
 import phone from "../assets/phone-icon.png";
 import mail from "../assets/mail-icon.png";
+// import {require} from "react";
 import "./ContactCard.css";
 
-export default function ContactCard() {
+export default function ContactCard({img, name, phoneNumber, email}) {
+    // Note: URL needed for Vite
+    // https://youtu.be/7EFZIe_Cpv8?feature=shared&t=698
+    const url = new URL(`${img}`, import.meta.url);
     return (
         <div className="contact-card">
-            <img src={whiskerson}/>
-            <h3>Mr. Whiskerson</h3>
+            <img src={url.href}/>
+            <h3>{name}</h3>
             <div className="info-group">
                 <img src={phone}/>
-                <p>(212) 555-1234</p>
+                <p>{phoneNumber}</p>
             </div>
             <div className="info-group">
                 <img src={mail}/>
-                <p>mr.whiskaz@catnap.meow</p>
+                <p>{email}</p>
             </div>
         </div>
     );
