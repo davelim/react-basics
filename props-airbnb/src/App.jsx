a7bc6b72c6080a6673c7fe5a4e449b8bbaec1ce9
Dave Lim <davelim77@gmail.com>
2024-07-31 09:30:11 -0500

    Remove practice (cat) cards

--- props-airbnb/src/App.jsx
+++ props-airbnb/src/App.jsx
@@ -1,7 +1,6 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Card from "./components/Card";
-import ContactCard from "./components/ContactCard";
 
 import './App.css';
 
@@ -13,28 +12,6 @@ function App() {
       <Navbar />
       <Hero />
       <Card />
-      <div className="contacts">
-        <ContactCard
-          img="../assets/mr-whiskerson.jpg"
-          name="Mr. Whiskerson"
-          phoneNumber="(212) 555-1234"
-          email="mr.whiskaz@catnap.meow"/>
-        <ContactCard
-          img="../assets/fluffykins.jpg"
-          name="Fluffykins"
-          phoneNumber="(212) 555-2345"
-          email="fluff@me.com"/>
-        <ContactCard
-          img="../assets/felix.jpg"
-          name="Felix"
-          phoneNumber="(212) 555-4567"
-          email="thecat@hotmail.com"/>
-        <ContactCard
-          img="../assets/pumpkin.png"
-          name="Pumpkin"
-          phoneNumber="(0800) CAT KING"
-          email="pumpkin@scrimba.com"/>
-      </div>
     </div>
   )
 }
