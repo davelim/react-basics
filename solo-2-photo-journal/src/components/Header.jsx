e197b0656aeb5062a87194daddc83818e026d4b5
Dave Lim <davelim77@gmail.com>
2024-08-02 14:58:39 -0500

    Styling for header

--- solo-2-photo-journal/src/components/Header.jsx
+++ solo-2-photo-journal/src/components/Header.jsx
@@ -1,8 +1,8 @@
 export default function Header() {
     return (
         <header>
-            <i class="fa fa-globe"></i>
-            <h4>my travel journal</h4>
+            <i className="fa fa-globe"></i>
+            <span>my travel journal</span>
         </header>
     );
 }
