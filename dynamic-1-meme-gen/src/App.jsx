b1b45224d159af49f0344ed6f78a3f4361be07a4
Dave Lim <davelim77@gmail.com>
2024-08-05 16:08:57 -0500

    Intermediate challenge
    - .map() array to JSX, then render
    - add element to array on click

--- dynamic-1-meme-gen/src/App.jsx
+++ dynamic-1-meme-gen/src/App.jsx
@@ -3,10 +3,22 @@ import Meme from "./components/Meme";
 import './App.css'
 
 function App() {
+  let n = 3;
+  const thingsArray = ["Thing 1", "Thing 2"];
+
+  function handleClick() {
+    thingsArray.push(`Thing ${n++}`);
+    console.log(thingsArray);
+  }
+
+  const thingsJSX = thingsArray.map((t, i) => <p key={i}>{t}</p>);
+
   return (
     <div className="app">
       <Header />
       <Meme />
+      <button onClick={handleClick}>Add Item</button>
+      {thingsJSX}
     </div>
   )
 }
