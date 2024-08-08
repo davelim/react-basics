4555791d688743c411db119abc14ef840075b984
Dave Lim <davelim77@gmail.com>
2024-08-08 14:43:48 -0500

    Clean up useEffect()

--- use-effect/src/WindowTracker.jsx
+++ use-effect/src/WindowTracker.jsx
@@ -4,9 +4,21 @@ export default function WindowTracker() {
     const [windowWidth, setWindowWidth] = useState(window.innerWidth);
 
     useEffect(() => {
-        window.addEventListener("resize", () => {
+        function watchWidth() {
+            console.log("Set width");
             setWindowWidth(window.innerWidth);
-        })
+        }
+        window.addEventListener("resize", watchWidth);
+
+        /* Best practice: function passed into useEffect returns a function
+           that cleans up side affects it created. In this case:
+           window.removeEventListener() is the clean up function of
+           widow.addEventListener().
+        */
+        return () => {
+            console.log("Clean up");
+            window.removeEventListener("resize", watchWidth);
+        }
     }, []);
 
     return (
