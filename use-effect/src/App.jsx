ec8e128a7f3d466be6a6361d5c682047187a2cce
Dave Lim <davelim77@gmail.com>
2024-08-08 14:21:32 -0500

    Toggle WindowTracker component

--- use-effect/src/App.jsx
+++ use-effect/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react';
+import WindowTracker from "./WindowTracker";
 import './App.css'
 
 function App() {
@@ -18,11 +19,21 @@ function App() {
       .then(data => setStarWarsData(data));
   }, [count]);
 
+  const [show, setShow] = useState(true);
+  function toggle() {
+    setShow(prev => !prev);
+  }
+
   return (
     <div>
       <p>The count is {count}</p>
       <button onClick={handleClick}>Get Next Character</button>
       <pre>{JSON.stringify(starWarsData, null, 2)}</pre>
+      <hr/>
+      <button onClick={toggle}>
+        Toggle WindowTracer
+      </button>
+      {show && <WindowTracker />}
     </div>
   )
 }
---
+++ use-effect/src/WindowTracker.jsx
@@ -0,0 +1,5 @@
+export default function WindowTracker() {
+    return (
+        <h1>Window width: {window.innerWidth}</h1>
+    );
+}
