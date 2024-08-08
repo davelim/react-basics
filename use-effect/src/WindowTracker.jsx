7bacaf673e20cb894e3b2eee10b5b3db638ea1d2
Dave Lim <davelim77@gmail.com>
2024-08-08 14:30:27 -0500

    React.useEffect() to handle window resize
    Note: this implementation causes a memory leak when WindowTracker
    component is "unmounted", i.e. when "show" (in App.jsx) is false, and
    WindowTracker component is not rendered.

--- use-effect/src/WindowTracker.jsx
+++ use-effect/src/WindowTracker.jsx
@@ -1,5 +1,15 @@
+import {useState, useEffect} from "react";
+
 export default function WindowTracker() {
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+    useEffect(() => {
+        window.addEventListener("resize", () => {
+            setWindowWidth(window.innerWidth);
+        })
+    }, []);
+
     return (
-        <h1>Window width: {window.innerWidth}</h1>
+        <h1>Window width: {windowWidth}</h1>
     );
 }
