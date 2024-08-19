e85dc743acaaacbb374bb58e2813cc00d6cc16e2
Dave Lim <davelim77@gmail.com>
2024-08-19 16:46:04 -0500

    Adjustments after review

--- tenzies-final-project/src/components/Die.jsx
+++ tenzies-final-project/src/components/Die.jsx
@@ -1,7 +1,7 @@
 export default function Die(props) {
     return(
         <div className="die">
-            <p>{props.value}</p>
+            <h2 className="die-num">{props.value}</h2>
         </div>
     )
 }
