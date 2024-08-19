328ff964e0eb19826274be2bce18bbff33ae8109
Dave Lim <davelim77@gmail.com>
2024-08-19 13:23:02 -0500

    First commit for tenzies
    New Vite project + some starter code & comments.

---
+++ tenzies-final-project/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import './App.css'
+
+/*
+ * Challenge:
+ * - create a separate App component
+ * - import and rende the app component here
+ * - in app component, render <main> element
+ * - style everything to look like the slide
+ */
+function App() {
+
+  return (
+    <main>
+      main
+    </main>
+  )
+}
+
+export default App
