59e9e2be3793607e05eee7c0faa1e3ca2bdde4af
Dave Lim <davelim77@gmail.com>
2024-08-20 15:04:38 -0500

    Make state array of objects
    Convert state from array of numbers to array of objects. Object contains
    an isHeld attribute to indicate that the specific dice should not change
    on the next dice roll.

--- tenzies-final-project/src/App.jsx
+++ tenzies-final-project/src/App.jsx
@@ -1,26 +1,27 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import Die from "./components/Die.jsx";
 import './App.css'
 
 
 function allNewDice(max, min = 1, len = 10) {
-  return Array.from(
+  const numArr = Array.from(
     {length: len},
     () => Math.floor(Math.random() * (max - min + 1)) + min
   );
+  return numArr.map(n => {return {value: n, isHeld: false}});
 }
 function App() {
-  const [diceNumbers, setDiceNumbers] = useState(allNewDice(6));
-  let diceJSX = diceNumbers.map((n, idx) => <Die key={idx} value={n} />);
+  const [dice, setDice] = useState(allNewDice(6));
+  let diceElements = dice.map((d, idx) => <Die key={idx} value={d.value} />);
 
   function rollDice() {
-    setDiceNumbers(allNewDice(6));
+    setDice(allNewDice(6));
   }
 
   return (
     <main>
       <div className="dice">
-        {diceJSX}
+        {diceElements}
       </div>
       <button onClick={rollDice} className="roll-button">Roll</button>
     </main>
