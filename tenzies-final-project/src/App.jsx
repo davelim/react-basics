37e209a668cfb9c32c0780cbfeac87b664275ca9
Dave Lim <davelim77@gmail.com>
2024-08-21 16:25:14 -0500

    Mods after review
    Cleaner way of "holding" <Die /> - DRY out code a little with function
    to generate single die. Reuse function to generate dice that are not
    held.

--- tenzies-final-project/src/App.jsx
+++ tenzies-final-project/src/App.jsx
@@ -4,42 +4,39 @@ import Die from "./components/Die.jsx";
 import './App.css'
 
 
-function allNewDice(max, min = 1, len = 10) {
-  const numArr = Array.from(
-    {length: len},
-    () => Math.floor(Math.random() * (max - min + 1)) + min
-  );
-  return numArr.map(n => {return {
-    value: n,
+function generateNewDie() {
+  return {
+    value: Math.ceil(Math.random() * 6),
     isHeld: false,
     id: nanoid()
-  }});
+  }
+}
+function allNewDice() {
+  return Array.from(
+    {length: 6},
+    () => generateNewDie()
+  );
 }
 function App() {
   const [dice, setDice] = useState(allNewDice(6));
 
   function rollDice() {
-    // setDice(allNewDice(6));
-    const newDice = allNewDice(6);
-    setDice(oldDice => oldDice.map((d, idx) => {
-      return ({
-        ...(d.isHeld ? d : newDice[idx])
-      });
+    setDice(oldDice => oldDice.map(d => {
+      return d.isHeld
+        ? d
+        : generateNewDie()
     }));
   }
 
   function holdDie(event, id) {
-    console.log(id);
     event.stopPropagation();
     setDice(oldDice => oldDice.map(d => {
-      return ({
-        ...d,
-        isHeld: d.id === id ? !d.isHeld : d.isHeld
-      });
+      return d.id === id
+        ? {...d, isHeld: !d.isHeld}
+        : d;
     }));
   }
 
-  console.log(dice);
   const diceElements = dice.map(d =>
     <Die
       key={d.id}
